b31fff1b7190124d8f382d86cf97513f3b8e5eac
rollcorn <lazydaimon@gmail.com>
2022-07-24 22:50:24 +0300

    [2-Task] LinkedList refclection and fix first solution

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -10,7 +10,21 @@
  */
 public class LinkedList {
     class Node {}
+
+    // Статусы
+    int HEAD_STATUS;
+    int TAIL_STATUS;
+    int RIGHT_STATUS;
+    int PUT_RIGHT_STATUS;
+    int PUT_LEFT_STATUS;
+    int REMOVE_STATUS;
+    int REPLACE_STATUS;
+    int FIND_STATUS;
+    int GET_STATUS;
+
     // Конструктор:
+    // постусловие: создан новый пустой список
+    public LinkedList<T> LinkedList();
 
     /******************************************************************************************
      * Команды:
@@ -24,21 +38,27 @@ public class LinkedList {
     public void tail() { }
 
     // предусловие: справа есть элемент
+    // постусловие: курсор сдвинут на один узел вправо
     public void right() { }
 
-    // постусловие: перед текущим узлом новый узел с заданным значением
+    // предусловие: список не пуст;
+// постусловие: перед текущим узлом новый узел с заданным значением
     public void putLeft(Node item) { }
 
+    // предусловие: список не пуст;
     // постусловие: следом за текущим узлом новый узел с заданным значением
     public void putRight(Node item) { }
 
     // постусловие: в конце списка появляется новый элемент
     public void addTail(Node item) { }
 
+    // предусловие: список не пуст;
+    // постусловие: значение текущего узла заменено на новое
     public void replace(Node item) { }
 
-    // постусловие: курсор смещается к правому соседу, если он есть, в противном случае
-    //              курсор смещается к левому соседу, если он есть
+    // предусловие: список не пуст;
+    // постусловие: текущий узел удалён, курсор смещается к правому соседу, если он есть,
+    //              в противном случае курсор смещается к левому соседу, если он есть
     public void remove() { }
 
     // постусловие: в списке отсутствуют элементы равные item
@@ -47,20 +67,69 @@ public class LinkedList {
     // постусловие: список пуст
     public void clear() { }
 
+    // постусловие: курсор установлен на следующий узел
+    //              с искомым значением, если такой узел найден
+    public void find(Node item) { }
+
     /***************************************************************************************
      * Запросы:
      */
     public void get(Node item) { }
 
-    public void find(Node item) { }
+    public boolean isHead() { }
 
-    public void isHead() { }
+    public boolean isTail() { }
 
-    public void isTail() { }
+    public boolean isValue() { }
 
-    public void isValue() { }
-
-    // количество элементов в списке
     public int size() { }
+
+    /***************************************************************************************
+     * Запросы статусов:
+     */
+    // успешно; список пуст
+    public int getHeadStatus() {
+        return HEAD_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getTailStatus() {
+        return TAIL_STATUS;
+    }
+
+    // успешно; справа нет элемента
+    public int getRightStatus() {
+        return RIGHT_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getPutRightStatus() {
+        return PUT_RIGHT_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getPutLeftStatus() {
+        return PUT_LEFT_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getRemoveStatus() {
+        return REMOVE_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getReplaceStatus() {
+        return REPLACE_STATUS;
+    }
+
+    // элемент найден; элемент не найден; список пуст
+    public int getFindStatus() {
+        return FIND_STATUS;
+    }
+
+    // успешно; список пуст
+    public int getGetStatus() {
+        return GET_STATUS;
+    }
 
 }
